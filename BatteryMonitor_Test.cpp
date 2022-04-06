09da081212eb20c6e751417002c69f754518880b
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-04-06 12:00:02 +0530

    Update BatteryMonitor_Test.cpp

--- BatteryMonitor_Test.cpp
+++ BatteryMonitor_Test.cpp
@@ -16,7 +16,7 @@ TEST_CASE("Verify formatted output data for individual battery")
 {
   int Location=6;
   int BatteryPos=1;
-  char* FormatOPData;
+  char FormatOPData[100];
   ReadBatteryStatus( Location, BatteryPos, FormatOPData);
   REQUIRE(strcmp(FormatOPData,"B1CR:0.600000,,B1SOC:77,B1Temp:2") == 0);
 }
