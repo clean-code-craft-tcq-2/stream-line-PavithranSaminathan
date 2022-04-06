96e96865c89dddb7dcfa514c20bbbbc4d5a48255
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-04-06 14:28:46 +0530

    Update TransmitBatteryReport.h

--- TransmitBatteryReport.h
+++ TransmitBatteryReport.h
@@ -1,7 +1,7 @@
 #ifndef TransmitBatteryReport_h
 #define TransmitBatteryReport_h
 
-int PrintBatteryStatus(int NoofReadings, int TotalNoOfBattery);
+int TransmitBatteryStatus(int NoofReadings, int TotalNoOfBattery);
 void ReadBatteryStatus(int Location,int BatteryPos,char* FormatOPData);
 
 #endif
