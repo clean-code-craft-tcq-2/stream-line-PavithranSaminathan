6706e5ccc006ea399c597d34db021a4605133bbc
SathishKumar Loganathan <43195344+SathishKumar-Loganathan@users.noreply.github.com>
2022-04-22 02:32:08 +0530

    Update Receive_And_Compute_Statistics.cpp

--- Receiver/Receive_And_Compute_Statistics.cpp
+++ Receiver/Receive_And_Compute_Statistics.cpp
@@ -8,7 +8,7 @@ TEST_CASE("Tests to verify the calculate statistics") {
 	
 	float ExpectedChargeRate[][3] = {{0.60,1.10,0.8},{0.00,1.20,0.94}};
 	float ExpectedTemperature[][3] = {{5.00,45.00,15.80},{0.00,45.00,35.40}};
-	float ExpectedSOC[][3] = {{20.00,45.00,15.80},{21.00,80.00,51.00}};
+	float ExpectedSOC[][3] = {{20.00,80.00,15.80},{21.00,80.00,51.00}};
 		
 	for(int i=0; i<NUMBER_OF_BATTERY; i++)
 	{
