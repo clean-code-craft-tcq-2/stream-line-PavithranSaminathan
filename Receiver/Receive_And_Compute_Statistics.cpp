651910dd916a9c27f2bb3aab63c0bcc91c56c86e
SathishKumar Loganathan <43195344+SathishKumar-Loganathan@users.noreply.github.com>
2022-04-22 02:43:08 +0530

    Update Receive_And_Compute_Statistics.cpp

--- Receiver/Receive_And_Compute_Statistics.cpp
+++ Receiver/Receive_And_Compute_Statistics.cpp
@@ -6,7 +6,7 @@ TEST_CASE("Tests to verify the calculate statistics") {
 	struct Battery BatteryParameters;
 	ReceiverSideImplementation(&BatteryParameters);
 	
-	float ExpectedChargeRate[][3] = {{0.60,1.10,0.8},{0.00,1.20,0.94}};
+	float ExpectedChargeRate[][3] = {{0.60,1.10,0.8},{0.00,1.20,0.94f}};
 	float ExpectedTemperature[][3] = {{5.00,45.00,15.80},{0.00,45.00,35.40}};
 	float ExpectedSOC[][3] = {{20.00,80.00,51.20},{21.00,80.00,51.00}};
 		
